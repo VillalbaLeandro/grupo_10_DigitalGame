215dffad2c26dc56d3c42f9f7410ae311a5d47d8
VillalbaLeandro <107075681+VillalbaLeandro@users.noreply.github.com>
2022-08-04 22:55:01 -0300

    optimizando carpetas y rutas

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -4,8 +4,15 @@ const productosController = {
     detalle: (req, res) => {
         res.render(path.join(__dirname, "../views/products/productDetail.ejs"))
     },
-    crear: ()=>{},
-    editar: ()=>{}
+    crear: (req, res) => {
+        res.render(path.join(__dirname, "../views/admin/createProduct.ejs"))
+    },
+    editar: (req, res) => {
+        res.render(path.join(__dirname, "../views/admin/editProduct.ejs"))
+    },
+    carrito: (req, res) => {
+        res.render(path.join(__dirname, "../views/products/productCart.ejs"))
+    }
 
 
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,8 @@ const productosController = require ("../controllers/productsControllers.js")
 const router = express.Router()
 
 router.get ("/productDetail", productosController.detalle);
-
+router.get ("/createProduct", productosController.crear);
+router.get ("/editProduct", productosController.editar);
+router.get ("/productCart", productosController.carrito);
 
 module.exports= router;
