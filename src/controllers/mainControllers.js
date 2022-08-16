6818852a8758c8a9e50fc8323961a92a6cc9e2ea
VillalbaLeandro <exemprary@gmail.com>
2022-08-16 00:11:27 -0300

    no se si entran tantos cambios en un solo comentario: Se parametrizaron las rutas, hice  dinamico el ejs, hice el archivo JSON, se instalo el method override, se configuró, se agregaron imagenes al spiderman, se agregaron metodos a los controllers y se compartio el json en el mainControllers y en productControllers, necesito dormir aiuda

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,8 +1,13 @@
-const path = require("path")
+const path = require("path");
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 
 const mainControllers={
     home: (req, res) => {
-        res.render(path.join(__dirname, "../views/index.ejs"))
+        res.render("index", { products })
     },
     login: (req, res) => {
         res.render(path.join(__dirname, "../views/users/login.ejs"))
--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,8 +1,17 @@
 const path = require("path");
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const productosController = {
-    listado:()=>{},
+    listado:(req, res)=>{
+        
+
+    },
     detalle: (req, res) => {
-        res.render("products/productDetail");
+        const product = products.find((product)=> product.id == req.params.id)
+        res.render("products/productDetail", { product });
     },
     crear: (req, res) => {
         res.render("admin/createProduct.ejs");
