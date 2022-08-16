d9ae41c6ec6a0aaa09e9aede19260344768aac83
VillalbaLeandro <exemprary@gmail.com>
2022-08-16 17:02:54 -0300

    se creo el formulario de carga de producto. Se creo el controller con el JSON pero no lo pude hacer funcionar todavia. Se retocaron cosas en el css de todas las vistas

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -5,16 +5,59 @@ const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productosController = {
-    listado:(req, res)=>{
-        
+    listado: (req, res) => {
+
 
     },
     detalle: (req, res) => {
-        const product = products.find((product)=> product.id == req.params.id)
+        const product = products.find((product) => product.id == req.params.id)
         res.render("products/productDetail", { product });
     },
     crear: (req, res) => {
         res.render("admin/createProduct.ejs");
+    },
+    store: (req, res) => {
+
+        const productsClone = products
+        const newProduct = {
+            id: "",
+            name: req.body.nombre,
+            compatibility: req.body.compatibility,
+            gender: req.body.gender,
+            players: req.body.players,
+            price: req.body.price,
+            language: req.body.language,
+            releaseData: req.body.releaseData,
+            img: req.body.img,
+            fullName: req.body.fullName,
+            description: req.body.description,
+            capture1: req.body.capture1,
+            capture2: req.body.capture2,
+            capture3: req.body.capture3,
+            capture4: req.body.capture4,
+            video: req.body.video,
+            discount: req.body.discount,
+            freeShipping: req.body.freeShipping,
+            categori: "",
+        }
+
+        productsClone.push(newProduct);
+        fs.writeFileSync(JSON.stringify(productsClone, null, ""))
+        // // GUARDARLA   
+        // // leer que cosas ya habia
+        // let archivoProduct = fs.readFileSync("products.json", { encoding: "utf-8" });
+        // let product;
+        // if (!archivoProduct) {
+        //     product = [];
+        // } else {
+        //     product = JSON.parse(archivoProduct);
+        // }
+        // product.push(newProduct);
+
+        // usuariosJson = JSON.stringify(product);
+        // fs.writeFileSync("products.json", usuariosJson);
+
+        res.redirect("/");
     },
     editar: (req, res) => {
         res.render("admin/editProduct.ejs");
@@ -25,4 +68,4 @@ const productosController = {
 
 
 };
-module.exports=productosController;
+module.exports = productosController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,13 @@
-const express = require ("express");
-const productosController = require ("../controllers/productsControllers.js");
+const express = require("express");
+const productosController = require("../controllers/productsControllers.js");
 const router = express.Router();
 
-router.get ("/:id/productDetail", productosController.detalle); //Detalle de un producto particular
-router.get ("/createProduct", productosController.crear); //Formulario de creación de productos
-router.get ("/editProduct", productosController.editar); //Formulario de edición de productos
-router.put ("/editProduct", productosController.editar); //Acción de edición (a donde se envía el formulario):
-router.get ("/productCart", productosController.carrito);
+router.get("/:id/productDetail", productosController.detalle); //Detalle de un producto particular
+router.get("/createProduct", productosController.crear); //Formulario de creación de productos
+router.put("/createProduct", productosController.store); //accion creación de productos
+router.get("/editProduct", productosController.editar); //Formulario de edición de productos
+router.put("/editProduct", productosController.editar); //Acción de edición (a donde se envía el formulario):
+router.get("/productCart", productosController.carrito);
                                                         //Acción de borrado
                                                         //Listado de productos
 
