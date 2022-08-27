4b8e9df866915b2d337725cf7db4d211482c12a3
VillalbaLeandro <107075681+VillalbaLeandro@users.noreply.github.com>
2022-08-27 05:28:41 -0300

    configuracion de multer en routes/main.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 const methodOverride = require("method-override");
+const app = express();
 
 
 app.set("view engine", "ejs");
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,13 +1,22 @@
 const express = require ("express");
-const router = express.Router();
-
-
+const multer = require("multer");
+const path = require("path");
 const mainControllers = require("../controllers/mainControllers");
 
+const router = express.Router();
 
 router.get ("/", mainControllers.home);
 router.get ("/login", mainControllers.login);
-router.get ("/register", mainControllers.register);
+router.get ("/register", uploadFile.single("avatar"), mainControllers.register);
 router.get ("/ofertas", mainControllers.ofertas);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, "../public/image/usersAvatar");
+    },
+    filename: (req, file, cb)=>{
+        cb(null,`${Date.now()}_img_${path.extname(file.originalname)}`)}
+});
+const uploadFile= multer({ storage });
 
 module.exports=router;
