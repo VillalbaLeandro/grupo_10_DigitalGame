0291fd4d9d6f79b953671206809c954e4f921820
VillalbaLeandro <107075681+VillalbaLeandro@users.noreply.github.com>
2022-08-27 21:58:01 -0300

    configuracion de users.js para la implementacion de multer

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,22 +1,33 @@
-const express = require ("express");
+const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const mainControllers = require("../controllers/mainControllers");
 
 const router = express.Router();
 
-router.get ("/", mainControllers.home);
-router.get ("/login", mainControllers.login);
-router.get ("/register", uploadFile.single("avatar"), mainControllers.register);
-router.get ("/ofertas", mainControllers.ofertas);
-
+//configuraciones para carga de archivos con multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, "../public/image/usersAvatar");
+    //el destino, es decir, donde se va a guardar el archivo.
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../../public/image/usersAvatar"));
     },
-    filename: (req, file, cb)=>{
-        cb(null,`${Date.now()}_img_${path.extname(file.originalname)}`)}
-});
-const uploadFile= multer({ storage });
+    //configurar el nombre del archivo 
+    filename: (req, file, cb) => {
+            //con dateNow (Devuelve el número de milisegundos transcurridos 
+    //desde las 00:00:00 UTC del 1 de enero de 1970.) +  _img_ + 
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
+    }
 
-module.exports=router;
+});
+const uploadFile = multer({ storage });
+
+
+router.get("/", mainControllers.home);
+router.get("/login", mainControllers.login);
+router.get("/register", mainControllers.register);
+router.post("/register", uploadFile.single("img-usser"), mainControllers.register);
+
+router.get("/ofertas", mainControllers.ofertas);
+
+
+module.exports = router;
