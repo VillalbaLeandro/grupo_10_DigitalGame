7c71476f43899b834638be3104cc6ff4245f73d0
nacho <nachomenguez22@gmail.com>
2022-09-09 01:45:00 -0300

    modifico rutas de acceso de create y edit product

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,11 +2,14 @@ const express = require("express");
 const productosController = require("../controllers/productsControllers.js");
 const router = express.Router();
 
+//MIDDLEWARES
+const guestMiddleware = require('../middlewares/guestMiddleware');
+
 router.get("/:id/productDetail", productosController.detalle); //Detalle de un producto particular
 router.get("/createProduct", productosController.crear); //Formulario de creación de productos
-router.put("/createProduct", productosController.store); //accion creación de productos
+router.put("/createProduct", guestMiddleware, productosController.store); //accion creación de productos
 router.get("/:id/editProduct", productosController.editar); //Formulario de edición de productos
-router.put("/:id/editProduct", productosController.update); //Acción de edición (a donde se envía el formulario):
+router.put("/:id/editProduct", guestMiddleware, productosController.update); //Acción de edición (a donde se envía el formulario):
 router.get("/productCart", productosController.carrito);
 router.delete('/:id/delete', productosController.destroy);  //Acción de borrado
                                                         //Listado de productos
