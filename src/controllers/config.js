b1fc98cd57f21d459755c882f3a67342d4fe6fe4
VillalbaLeandro <exemprary@gmail.com>
2023-07-12 07:19:20 -0300

    Probando deploy en webhost000

--- src/controllers/config.js
+++ src/controllers/config.js
@@ -1,7 +1,7 @@
 const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || null;
-const DB_NAME = process.env.DB_NAME || 'digitalgames_db';
+const DB_USER = "id20703711_leandrovillalba"
+const DB_PASSWORD = "37583803Lea!";
+const DB_NAME = "id20703711_digitalgames_db";
 const DB_PORT = process.env.DB_PORT || 3306;
 
 module.exports = {
